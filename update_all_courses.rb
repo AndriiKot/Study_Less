5556d84fcfaac8816fb3f690796d31b105d5c759
AndriiKot <a_kocyuba@ukr.net>
2024-07-10 08:57:50 +0200

    'Study Less' 'My personal data' !! updata course !!

---
+++ update_all_courses.rb
@@ -0,0 +1,11 @@
+﻿projects = {
+  "My_Personal_Data": "https://github.com/AndriiKot/My_Personal_Data.git",
+}
+
+update = "git subtree pull --prefix "
+
+projects.each do |folder, git_repo| 
+    system(`#{update}#{folder} #{git_repo} main`)
+end
+
+system(`git push`)
